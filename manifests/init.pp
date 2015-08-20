1f53efd866bda9c4720cfc069c476fdd67f08ab9
desalvo <Alessandro.DeSalvo@roma1.infn.it>
2015-08-20 13:59:16 +0200

    Fix repo rpm URL

--- manifests/init.pp
+++ manifests/init.pp
@@ -4,7 +4,7 @@ class cvmfs {
             package { "cvmfs-release":
                 ensure => installed,
                 provider => rpm,
-                source => "http://cvmrepo.web.cern.ch/cvmrepo/yum/cvmfs/EL/${operatingsystemmajrelease}/x86_64/cvmfs-release-2-4.el${operatingsystemmajrelease}.noarch.rpm",
+                source => "http://cvmrepo.web.cern.ch/cvmrepo/yum/cvmfs-release-2-5.noarch.rpm",
             }
         }
         default: {
